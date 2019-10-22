49b5846723d17f674bfc58974fccfbb163df0d6b
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-10-22 00:01:10 -0400

    refined

--- src/components/Render.jsx
+++ src/components/Render.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 export default function Render(props) {
@@ -7,18 +7,35 @@ export default function Render(props) {
   return (
 
     <div>
-       <h1>Get Active</h1>
-      <button id='recreational' onChange={props.handleChange} onClick={props.handleClick}>Recreational</button>{
-        <h1>{props.activity}</h1>
+      <h1>Get Active</h1>
+      {
+        <div>
+          <h1>{props.activity}</h1>
+          <p>{props.type}</p>
+        </div>
       }
-      
-      <button id='social' onChange={props.handleChange} onClick={props.handleClick}>Social</button>{
-         <h1>{props.type}</h1>
-        
-        }
+      <div className='button-grp'>
+        <button id='recreational' onClick={props.handleClick}>Recreational</button>
+
+        <button id='social' onClick={props.handleClick}>Social</button>
+
+        <button id='relaxation' onClick={props.handleClick}>Relaxation</button>
+
+        <button id='cooking' onClick={props.handleClick}>Cooking</button>
+
+        <button id='education' onClick={props.handleClick}>Education</button>
+
+        <button id='busywork' onClick={props.handleClick}>Busy Work</button>
+
+        <button id='charity' onClick={props.handleClick}>Charity</button>
+
+        <button id='music' onClick={props.handleClick}>Music</button>
+
+      </div>
+
       {/* <h1>{props.activity}</h1>
       <p>{props.type}</p> */}
-      </div>
+    </div>
   )
 
 
