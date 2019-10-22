ded2bbe9fe91629434be05269e048956cd78dcb0
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-10-22 18:38:43 -0400

    style home

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -5,14 +5,15 @@ import Activity from './Activity';
 import Relax from './Relax';
 import About from './About';
 import Footer from './Footer';
+import image from '../images/bghome.jpg'
 
 export default function Site() {
   return (
     <div>
-      
     
+    <Nav />
 
-
+    <img id="bg-img" src={image} alt="boat on water" width='100%' height='100%' />
 
 
 
