3a1c69aaf0f59b905f4dc9caabf5e4f5b42588f4
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-10-23 21:20:44 -0400

    sound buttons

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import music from '../assets/relaxmusic.mp3';
+import rain from '../assets/rain.mp3';
+import energy from '../assets/upbeat.mp3';
 
 
-class Sound extends React.Component{
+class Sound extends React.Component {
 
   state = {
     play: false
   }
+
 
   audio = new Audio(music)
 
@@ -14,18 +17,43 @@ class Sound extends React.Component{
     this.setState({
       play: !this.state.play
     }, () => {
-        this.state.play ? this.audio.play() : this.audio.pause()
-    
+      this.state.play ? this.audio.play() : this.audio.pause()
+
+    })
+  }
+
+  audioRain = new Audio(rain)
+
+  toggleOnRain = () => {
+    this.setState({
+      play: !this.state.play
+    }, () => {
+      this.state.play ? this.audioRain.play() : this.audioRain.pause()
+
+    })
+  }
+
+  audioEnergy = new Audio(energy)
+
+  toggleOnEnergy = () => {
+    this.setState({
+      play: !this.state.play
+    }, () => {
+      this.state.play ? this.audioEnergy.play() : this.audioEnergy.pause()
+
     })
   }
 
 
 
+
   render() {
     return (
-      
-      <div>
-        <button id='relax-btn' onClick={this.toggleOn} >{this.state.play ? 'Pause' : 'Relax' }</button>
+      <div className='sound-btn'>
+        <button id='relax-btn' onClick={this.toggleOn} >{this.state.play ? 'Pause' : 'Relax'}</button>
+        <button id='rain-btn' onClick={this.toggleOnRain} >{this.state.play ? 'Pause' : 'Rain'}</button>
+        <button id='energy-btn' onClick={this.toggleOnEnergy} >{this.state.play ? 'Pause' : 'Energy'}</button>
+
       </div>
     )
   }
